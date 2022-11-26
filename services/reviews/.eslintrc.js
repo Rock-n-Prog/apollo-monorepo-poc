8289d24d675621fcbd987eb3376bea9bba3854e3
Fabien Roy <fabienroy28@gmail.com>
2022-11-26 17:02:47 -0500

    fix: lint

--- services/reviews/.eslintrc.js
+++ services/reviews/.eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: ['@acme/eslint-config/node.js'],
+  rules: {
+    // Fix for codegen. Once the API is deployed, run codegen in CI and remove this override.
+    'import/no-unresolved': 'off'
+  }
 };
